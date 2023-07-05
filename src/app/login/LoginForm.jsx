5efe383b3b88995ce8fa22811f7bdac63eb40c67
Jeniffer Giraldo <tris@Jeniffers-MacBook-Air-2.local>
2023-07-05 14:56:37 -0400

    ADDED login and authentication

--- src/app/login/LoginForm.jsx
+++ src/app/login/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"; 
 import { useNavigate } from "react-router-dom";
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { AuthContext } from "../../App";
 
 const firebaseConfig = {
@@ -16,7 +16,7 @@ const firebaseConfig = {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
 
-export default function SignUpForm() { 
+export default function LoginForm() { 
 
     const { setUser } = useContext(AuthContext);
     const navigate = useNavigate();
@@ -31,11 +31,11 @@ export default function SignUpForm() {
     .catch(err => alert(err.message))
     }
 
-    const handleSignup = (e) => {
+    const handleLogin = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        createUserWithEmailAndPassword(auth, email, password)
+        signInWithEmailAndPassword(auth, email, password)
             .then(response => {
                 setUser(response.user);
                 navigate("/");
@@ -45,7 +45,7 @@ export default function SignUpForm() {
 
     return (
         <main>
-        <form onSubmit={handleSignup}> 
+        <form onSubmit={handleLogin}> 
          <label htmlFor="email">
             Email 
             <input type="email" name="email" />
@@ -56,9 +56,9 @@ export default function SignUpForm() {
             <input type="password" name="password" />
             </label>
             <br />
-            <input type="submit" value="Sign Up" />
+            <input type="submit" value="Login" />
         </form>
-        <button onClick={handleGoogle}>Signup with Google</button>
+        <button onClick={handleGoogle}>Login with Google</button>
         </main>
     );
 }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import LoginForm from "./LoginForm";
 export default function Login() {
     return (
         <main>
             <h1>Login</h1>
+            <LoginForm />
             Not a user? <Link to="/signup">Sign Up</Link>
         </main>
     )
